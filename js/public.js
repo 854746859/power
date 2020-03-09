b1bcfc8465c89b2eeb793641d43857016450742d
854746859 <854746859@qq.com>
2020-03-09 09:39:43 +0800

    editor

--- js/public.js
+++ js/public.js
@@ -1,16 +1,16 @@
 
 // 菜单js
 $(function(){
-	var n=$(".top_nav ul li.cur").index();
+	var n=$(".top_nav ul li.selet").index();
 	$(".top_nav ul li").mouseenter(
 		function(){
-			$(this).addClass("cur")
-			$(this).siblings().removeClass("cur")
+			$(this).addClass("selet")
+			$(this).siblings().removeClass("selet")
 		}
 	).mouseleave(
 		function(){
-			$(".top_nav ul li").eq(n).siblings().removeClass("cur");
-			$(".top_nav ul li").eq(n).addClass("cur")
+			$(".top_nav ul li").eq(n).siblings().removeClass("selet");
+			$(".top_nav ul li").eq(n).addClass("selet")
 		}
 	)
 })
