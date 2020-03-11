bc645b917f41fb3c4bca253b1bc0486443c02be9
854746859 <854746859@qq.com>
2020-03-11 21:00:44 +0800

    login

--- config.js
+++ config.js
@@ -6,4 +6,26 @@ function getUrldata(name) {
     var r = window.location.search.substr(1).match(reg); 
     if (r != null) return unescape(r[2]); 
     return null; 
-} 
+}
+//get token
+function GetToken(){
+	$.ajax({
+		type:"post",
+		url:sever_url+"login",
+		async:true,
+		data:{
+			user_account: 'admin',
+			user_password: '123456'
+		},
+		success:function(data){
+			if(data.code=='200'){
+				var obj = JSON.stringify(data.data);
+				sessionStorage.setItem("data",obj);
+				self.location = document.referrer;
+			}
+		},
+		error:function(){
+			
+		}
+	});
+}
