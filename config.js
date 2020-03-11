e947bda7d376861660d979c17ddce9aa1bede9c0
854746859 <854746859@qq.com>
2020-03-11 10:08:32 +0800

    login

--- config.js
+++ config.js
@@ -1,2 +1,9 @@
 //服务器地址
 var  sever_url = "http://114.55.35.82/tp6_forum/public/index.php/";
+//获取URL传值
+function getUrldata(name) { 
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i"); 
+    var r = window.location.search.substr(1).match(reg); 
+    if (r != null) return unescape(r[2]); 
+    return null; 
+} 
